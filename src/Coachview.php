480fdb30e8bf8d18b0dea4475ebc3b735cc0ae67
Robin van Schuilenburg <robin@diezit.nl>
2022-05-19 14:16:31 +0200

    Added publicly accessible authenticate method
    Method used to verify if we can authenticate with the coachview backend.

--- src/Coachview.php
+++ src/Coachview.php
@@ -13,6 +13,7 @@ use Diezit\CoachviewConnector\Classes\Teacher;
 use Diezit\CoachviewConnector\Classes\WebAanvraag;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 
@@ -84,25 +85,17 @@ class Coachview
                 $this->refreshAccessToken();
                 return $this->doRequest($endpoint, $method, $params, true);
             }
-
-            // @TODO: remove below Exception when going live. This is to make errors more verbose while testing.
-            if (!app()->environment('production')) {
-                dd($exception);
-            }
             return null;
         }
     }
 
-    private function getAccessToken(): ?string
-    {
-        if (!Cache::has(self::CACHE_KEY_ACCESS_TOKEN)) {
-            $this->refreshAccessToken();
-        }
-
-        return Cache::get(self::CACHE_KEY_ACCESS_TOKEN);
-    }
-
-    protected function refreshAccessToken(): void
+    /**
+     * Returns token data object containing an access token and its expiry time in milliseconds
+     *
+     * @return object string access_token, int expires_in
+     * @throws \Exception
+     */
+    public function authenticate(): object
     {
         try {
             $tokenRequest = $this->client->request(
@@ -132,12 +125,26 @@ class Coachview
                     ]
                 ]
             );
-        } catch (RequestException $exception) {
+        } catch (GuzzleException | ClientException | RequestException $exception) {
             throw new \Exception('Unable to retrieve authentication token: '.$exception->getMessage());
         }
 
         $tokenResponseBody = (string)$tokenRequest->getBody();
-        $tokenData = json_decode($tokenResponseBody);
+        return json_decode($tokenResponseBody);
+    }
+
+    private function getAccessToken(): ?string
+    {
+        if (!Cache::has(self::CACHE_KEY_ACCESS_TOKEN)) {
+            $this->refreshAccessToken();
+        }
+
+        return Cache::get(self::CACHE_KEY_ACCESS_TOKEN);
+    }
+
+    protected function refreshAccessToken(): void
+    {
+        $tokenData = $this->authenticate();
 
         Cache::put(self::CACHE_KEY_ACCESS_TOKEN, $tokenData->access_token, $tokenData->expires_in);
     }
